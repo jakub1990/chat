b797a93e95b7a1478e07ebb742514557f21fef38
Jakub Rozanski <jakub.rozanski1990@gmail.com>
2026-01-07 18:30:54 +0100

    History limit added

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ api_key = os.getenv("OPENAI_API_KEY")
 openai = OpenAI(api_key=api_key)
 history= []
 
+MAX_HISTORY = 100
+
+def trim_history(history):
+    return history[-MAX_HISTORY:]
+
 def get_ai_response(prompt, history):
   system_msg = {"role": "system", "content": "You are a helpful assistant."}
   user_msg = {"role": "user", "content": prompt}
   completion = openai.chat.completions.create(
   model="gpt-4.1-nano",
-  messages=[system_msg] + history + [user_msg],
+  messages=[system_msg] + trim_history(history) + [user_msg],
   temperature= 1.0)
   
   return completion.choices[0].message.content
-
-fun_fact = get_ai_response("Give me funny fact about Poland", history)
-
-print(fun_fact)
 
 while True:
     prompt = input("Ask the question for AI ")

3183594e5ae61c2d51a4e0371c702314ab6e6b40
Jakub Rozanski <jakub.rozanski1990@gmail.com>
2026-01-07 18:37:29 +0100

    History json save added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from openai import OpenAI
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
@@ -11,6 +12,10 @@ MAX_HISTORY = 100
 
 def trim_history(history):
     return history[-MAX_HISTORY:]
+
+def save_history(history, filename="history.json"):
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(history, f, ensure_ascii=False, indent=2)
 
 def get_ai_response(prompt, history):
   system_msg = {"role": "system", "content": "You are a helpful assistant."}
@@ -25,6 +30,7 @@ def get_ai_response(prompt, history):
 while True:
     prompt = input("Ask the question for AI ")
     if prompt == "quit":
+        save_history(history)
         break
 
     answer = get_ai_response(prompt, history)
